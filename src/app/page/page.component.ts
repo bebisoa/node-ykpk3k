4beb4cd55d474e3693c5e4e18d75c43480ae72f7
bebisoa <razafyrabe1@gmail.com>
2022-05-17 11:05:58 +0200

    1 binaries uploaded

--- src/app/page/page.component.ts
+++ src/app/page/page.component.ts
@@ -3,18 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-page',
   templateUrl: './page.component.html',
-  styleUrls: ['./page.component.css']
+  styleUrls: ['./page.component.css'],
 })
 export class PageComponent implements OnInit {
   page = {
-    title : 'Bienvenue sur 02 Celsius',
+    title: 'Bienvenue sur 02 Celsius',
     subtitle: 'Bienvenue!',
-    content: 'Suivez votre température et taux d'O2 quotidien.',
-    image: ''
-  }  
-  constructor() { }
+    content: 'Suivez votre température et taux oxygène',
+    image: 'assets/logo.jpg',
+  };
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
